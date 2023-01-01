efea4af0ee5e662eb1d3f6ab4de2b11d0a4c2dde
Andrew Li <54134293+andrewleaf22@users.noreply.github.com>
2022-12-31 23:46:23 -0600

    changed key on a component to not have index

--- components/other/FlightTable.tsx
+++ components/other/FlightTable.tsx
@@ -4,8 +4,8 @@ import Debate from "../../types/Debate";
 export const FlightTable: FunctionComponent<{rounds: Array<Debate>}> = ({rounds}) => {
     return (
       <>
-        {rounds.map((item, index) => (
-          <tr key={index} style={{height: "1rem"}}>
+        {rounds.map(item => (
+          <tr key={`${item.judgeName}${item.teamA}${item.teamB}${item.flight}`} style={{height: "1rem"}}>
             <td style={{width:"10%"}}>{item.flight}</td><td style={{width:"20%"}}>{item.teamA}</td><td style={{width:"20%"}}>{item.teamB}</td><td style={{width:"25%"}}>{item.roomCode}</td><td style={{width:"25%"}}>{item.judgeName}</td>
           </tr>
         ))}
